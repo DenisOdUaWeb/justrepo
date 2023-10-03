249196b297b510e1bb5643a9d413286755247c42
Denis <denis.od.ua.web@gmail.com>
2023-10-03 16:22:08 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    hellow World !
+    <h1>asdfg h1 </h1>
+    <section>
+        Section 
+    </section>
+    <div class="mb-3">div with margin bottom 3</div>
+</body>
+</html>
